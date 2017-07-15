e42292fe3b6c1f0cbdf9d1774277a749dccf1485
Chris <chris.hoffmann84@gmail.com>
2017-07-15 22:01:49 +0200

    - added a intercept handler for http requests - printed some infos about the request to the console - modified the result with some test data

--- short-lnk/server/main.js
+++ short-lnk/server/main.js
@@ -1,9 +1,27 @@
 import { Meteor } from 'meteor/meteor';
-import './../imports/api/users';
 import { LinksCollection } from './../imports/api/Links';
+import { WebApp } from 'meteor/webapp';
+
+import './../imports/api/users';
 import './../imports/startup/simple-schema-configuration';
 
-
-
 Meteor.startup(() => {
+  WebApp.connectHandlers.use((req, res, next) => {
+    console.log('This is from my custom middleware!');
+    console.log(req.url, req.method, req.headers, req.query);
+    
+    //set http status code
+    res.statusCode = 404;
+
+    //set http headers
+    res.setHeader('my-custom-header', 'chris was here :-)');
+
+    //set http body
+    res.write('<h1>This is MY house!!!</h1>');
+
+    //end http request
+    res.end();
+
+    next();
+  });
 });
